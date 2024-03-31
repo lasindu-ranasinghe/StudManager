5117cb42ee664bd70a640d43f078bfa721831464
lasindu-ranasinghe <116148700+lasindu-ranasinghe@users.noreply.github.com>
2024-03-31 20:44:07 +0530

    Backend: StudentRepo and StudentServices are Created.

--- backend/src/main/java/com/example/backend/repo/StudentRepo.java
+++ backend/src/main/java/com/example/backend/repo/StudentRepo.java
@@ -1,4 +1,12 @@
 package com.example.backend.repo;
 
-public interface StudentRepo {
+import com.example.backend.entity.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudentRepo extends JpaRepository<Student,String> {
+    boolean existsByStudentNIC(String studentNIC);
+    boolean existsByStudentRegNo(String studentRegNo);
+    Student findByStudentRegNo(String studentRegNo);
+    void deleteByStudentRegNo(String studentRegNo);
+
 }
--- backend/src/main/java/com/example/backend/service/StudentServices.java
+++ backend/src/main/java/com/example/backend/service/StudentServices.java
@@ -1,3 +1,88 @@
 package com.example.backend.service;
+
+import com.example.backend.dto.StudentDTO;
+import com.example.backend.entity.Student;
+import com.example.backend.repo.StudentRepo;
+import com.example.backend.util.VarList;
+import jakarta.transaction.Transactional;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
+@Transactional
 public class StudentServices {
+
+    @Autowired
+    private StudentRepo studentRepo;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+
+    public Map<String, Object> saveStudent(StudentDTO studentDTO){
+        Map<String, Object> response = new HashMap<>();
+
+        if (studentRepo.existsByStudentNIC(studentDTO.getStudentNIC())){
+            response.put("status", VarList.RSP_DUPLICATED);
+        }else {
+            //Mapping the DTO and Entity
+            Student entity = studentRepo.save(modelMapper.map(studentDTO, Student.class));
+            //Updating Reg No
+            Integer maxStudentId = entity.getStudentId();
+            String customId = studentDTO.getStudentIntake() + "-" + studentDTO.getStudentDegreeCode() + "-" + (maxStudentId);
+            entity.setStudentRegNo(customId);
+            studentRepo.save(entity);
+
+            // Setting response
+            response.put("status", VarList.RSP_SUCCESS);
+            response.put("customId", customId);
+        }
+        return response;
+    }
+    public List<StudentDTO> getAllEmployee(){
+        List<Student> employeeList = studentRepo.findAll();
+        return modelMapper.map(employeeList,new TypeToken<ArrayList<StudentDTO>>(){
+        }.getType());
+    }
+
+    public StudentDTO searchStudent(String studentRegNo){
+        if (studentRepo.existsByStudentRegNo(studentRegNo)){
+            Student student =studentRepo.findByStudentRegNo(studentRegNo);
+            return modelMapper.map(student,StudentDTO.class);
+        }else {
+            return null;
+        }
+    }
+
+    public String updateStudent(StudentDTO studentDTO) {
+        String studentRegNo = studentDTO.getStudentRegNo();
+        if (studentRegNo != null && !studentRegNo.isEmpty()) {
+            if (studentRepo.existsByStudentRegNo(studentRegNo)) {
+                Student existingStudent = studentRepo.findByStudentRegNo(studentRegNo);
+                // Update existing student entity with values from studentDTO
+                modelMapper.map(studentDTO, existingStudent);
+                studentRepo.save(existingStudent);
+                return VarList.RSP_SUCCESS;
+            } else {
+                return VarList.RSP_NO_DATA_FOUND;
+            }
+        } else {
+            return VarList.RSP_ERROR;
+        }
+    }
+    public String deleteEmployee(String studentRegNo){
+        if (studentRepo.existsByStudentRegNo(studentRegNo)){
+            studentRepo.deleteByStudentRegNo(studentRegNo);
+            return VarList.RSP_SUCCESS;
+        }else {
+            return VarList.RSP_NO_DATA_FOUND;
+        }
+    }
 }
