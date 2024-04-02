8b6b41f46434cd8036ac4b9d1508276cead7fde1
lasindu-ranasinghe <116148700+lasindu-ranasinghe@users.noreply.github.com>
2024-04-02 14:19:12 +0530

    Backend: Log files created

--- backend/src/main/java/com/example/backend/Controller/StudentControllers.java
+++ backend/src/main/java/com/example/backend/Controller/StudentControllers.java
@@ -175,7 +175,6 @@ public class StudentControllers {
         }
     }
 
-    }
 
     @DeleteMapping("/deleteStudent/{studentRegNo}")
     public ResponseEntity deleteStudent(@PathVariable String studentRegNo) {
